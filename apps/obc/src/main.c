d751f10aca555495d9b362821e62f8751d41f02d
Shinjo, Mirai <oss@mshinjo.com>
2025-02-22 15:37:13 -0500

    add minimal baseline OBC application
    Create a basic OBC application with CMake and source files.
    
    Signed-off-by: Mirai SHINJO <oss@mshinjo.com>

---
+++ apps/obc/src/main.c
@@ -0,0 +1,13 @@
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(obc);
+
+int main(void)
+{
+        while (1) {
+                LOG_INF("obc");
+                k_msleep(1000);
+        }
+        return 0;
+}
