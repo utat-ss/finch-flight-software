bd340996f281e36c21ba337c8cfa09521876020f
Mirai SHINJO <oss@mshinjo.com>
2025-05-17 12:51:35 -0400

    finch-flight-software-env.sh: add SPDX identifier and copyright notice
    Commit 2af85bcf5fe1d1c3f33f9e9a18c27b4444ac16cf introduced the
    checkpatch.pl script from the Linux kernel, which is distributed
    under the GPL-2.0 license, whereas this repository uses the Apache-2.0
    license at the root.
    This commit adds the appropriate SPDX identifier and copyright notice to
    relevant files for improved clarity.
    
    Signed-off-by: Mirai SHINJO <oss@mshinjo.com>

--- finch-flight-software-env.sh
+++ finch-flight-software-env.sh
@@ -1,3 +1,6 @@
+# Copyright (c) 2025 The FINCH CubeSat Project Flight Software Contributors
+# SPDX-License-Identifier: Apache-2.0
+
 if [ -n "$BASH_SOURCE" ]; then
     _script_path="${BASH_SOURCE[0]}"
 elif [ -n "$ZSH_VERSION" ]; then
