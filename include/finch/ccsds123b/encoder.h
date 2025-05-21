6fa6e408965dda6bd2b16721df6c2f01e261bce4
Alex Apostolu <apostolu240@gmail.com>
2025-05-20 22:05:13 -0400

    lib: Add CCSDS 123.0-B-2 image compression algorithm
    Add CCSDS 123.0-B-2 image predictor and various utility functions for
    image creation and math.
    
    Signed-off-by: Alex Apostolu <apostolu240@gmail.com>

---
+++ include/finch/ccsds123b/encoder.h
@@ -0,0 +1,24 @@
+#ifndef FINCH_INCLUDE_ENCODER_H
+#define FINCH_INCLUDE_ENCODER_H
+
+#include <finch/ccsds123b/util.h>
+#include <finch/ccsds123b/constants.h>
+
+/*
+ * [5.4.2.3] Band Sequential Order.
+ */
+void encode_prediction(
+	const vec3 * N,
+	int32_t prediction[N->z][N->y][N->x],
+	char encoding[N->z][N->y][N->x][32]
+);
+
+/*
+ * [5.4.3.2.2.1] Length limited Golomb-power-of-2 (GPO2)
+ */
+void encode_gpo2(
+	int j, int k,
+	char out[32]
+);
+
+#endif /* FINCH_INCLUDE_ENCODER_H */
---
+++ include/finch/ccsds123b/image.h
@@ -0,0 +1,14 @@
+#ifndef FINCH_INCLUDE_IMAGE_H
+#define FINCH_INCLUDE_IMAGE_H
+
+#include <finch/ccsds123b/util.h>
+
+void img_init(void);
+
+const vec3 *img_get_N(void);
+
+int img_get_pxl(int z, int y, int x);
+
+int img_get_pxl_t(int z, int t);
+
+#endif /* FINCH_INCLUDE_IMAGE_H */
