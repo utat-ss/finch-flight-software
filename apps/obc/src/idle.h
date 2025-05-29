2507ccd0d98621a4c1e6ea1813466a4a038843e2
Riaab Zahid <riaab.zahid@gmail.com>
2025-05-29 16:22:41 -0400

    Idle: changed idle.c and idle.h I reviewed the comments on the idle.c and idle.h files and corrected them. I also changed the logic of idle.c a bit
    Signed-off-by: Riaab Zahid <riaab.zahid@gmail.com>

--- apps/obc/src/idle.h
+++ apps/obc/src/idle.h
@@ -2,38 +2,26 @@
  * @brief Header for Idle Command Sequene Logic
 */
 
-#ifndef IDLE_H
-#define IDLE_H
+#ifndef idle_h
+#define idle_h
 
 #include <stdbool.h>
 
-/// Status type for command sequences
-typedef enum CommandStatus{
-    STATUS_SUCCESS,
-    STATUS_ERROR_COM
-} CommandStatus; 
-
-/// System modules
-typedef enum SystemModule{
+/**
+ * @brief system modules involved in idle command sequence 
+*/
+typedef enum system_module{
     MODULE_OPERATOR,
     MODULE_MCC_GS,
     MODULE_RF, 
     MODULE_OBC,
     MODULE_ADCS, 
     MODULE_PAY
-} SystemModule;
+} system_module; 
 
-CommandStatus ADCS_ExecuteOrient(const char* attitude);
-void OBC_SystemHealthCheck();
-bool OBC_CheckScheduled(void);
-void OBC_ScheduledModeChange(const char* mode, const char* params);
-void OBC_LogError(const char* errorType);
-void PAY_EnterMode(const char* mode);
-bool RF_TransmitCommand(const char* command, const char* params);
-void Operator_SendCommand(const char* command, const char* params);
-void IdleCommandSequence();
-
-
+/** 
+ * @brief executes the idle command sequence 
+*/
+int idle_command_sequence();
 
 #endif
-
