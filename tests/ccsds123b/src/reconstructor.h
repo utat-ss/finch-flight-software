49bc715727f71b5f14c26e62e3b4e86c805d1df2
Alex Apostolu <apostolu240@gmail.com>
2025-06-01 15:14:10 -0400

    tests: Add CCSDS 123.0-B-2 image compression tests
    Add image compression manual tests.
    
    Signed-off-by: Alex Apostolu <apostolu240@gmail.com>

---
+++ tests/ccsds123b/src/reconstructor.h
@@ -0,0 +1,35 @@
+#ifndef FINCH_INCLUDE_RECONSTRUCTOR_H
+#define FINCH_INCLUDE_RECONSTRUCTOR_H
+
+#include <finch/ccsds123b/predictor.h>
+#include <finch/ccsds123b/constants.h>
+#include <finch/ccsds123b/util.h>
+
+/*
+ * Outputs the predicted image.
+ */
+void decode_encoding(
+	const vec3 * N,
+	char encoding[N->z][N->y][N->x][32],
+	int prediction[N->z][N->y][N->x]
+);
+
+/*
+ * Decodes a single codeword.
+ */
+void decode_gpo2(
+	const char code[32],
+	int *j,
+	int *k
+);
+
+/*
+ * Outputs the original image.
+ */
+void reconstruct_prediction(
+	const vec3 * N,
+	int prediction[N->z][N->y][N->x],
+	int image[N->z][N->y][N->x]
+);
+
+#endif /* FINCH_INCLUDE_RECONSTRUCTOR_H */
