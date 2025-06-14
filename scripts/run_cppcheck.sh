6ab7d32520b6e062b98088887b6955bed75b9e56
Mirai SHINJO <oss@mshinjo.com>
2025-06-14 12:50:44 -0400

    scripts: run_cppcheck: prefer 4 spaces for indentation
    Align indentation style with other shell scripts under
    the scripts/ directory.
    
    Signed-off-by: Mirai SHINJO <oss@mshinjo.com>

--- scripts/run_cppcheck.sh
+++ scripts/run_cppcheck.sh
@@ -8,6 +8,6 @@ set -e
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../finch-flight-software-env.sh"
 
 cppcheck --enable=all \
-  --suppress=missingIncludeSystem \
-  --error-exitcode=1 \
-  "${FINCH_FLIGHT_SOFTWARE_ROOT}"
+    --suppress=missingIncludeSystem \
+    --error-exitcode=1 \
+    "${FINCH_FLIGHT_SOFTWARE_ROOT}"
