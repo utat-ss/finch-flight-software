bd04bb93e0eda22812eb7f30f0eb442a6260398b
Kevin_KyawThiha <kyaw.thiha@mail.utoronto.ca>
2025-06-14 13:04:20 -0400

    Fixed indent + added pyocd

--- boards/pay/board.cmake
+++ boards/pay/board.cmake
@@ -1,5 +1,6 @@
 # Copyright (c) 2025 The FINCH CubeSat Project Flight Software Contributors
 # SPDX-License-Identifier: Apache-2.0
 
-include(${ZEPHYR_BASE}/boards/common/openocd-stm32.board.cmake)
+board_runner_args(pyocd "--target=stm32h743zitx")
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 
