e84d9dd9caab984b3410178288cb22bf8d010bde
Jehan Billimoria <jehan.billimoria1@gmail.com>
2025-06-28 15:29:45 -0400

    Updated ADCS driver test code- updated the device binding, calling uart_poll_in only once, and updated testing to currently output the SNID of the dev board character by character.

--- jehan-ADCS/src/main.c
+++ jehan-ADCS/src/main.c
@@ -6,7 +6,8 @@
 #include <string.h>
 
 //const struct device *adcs_uart = DEVICE_DT_GET(DT_NODELABEL(uart0));
-const struct device *adcs_uart = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+const struct device *adcs_uart = DEVICE_DT_GET(DT_NODELABEL(usart1));
+
 
 void adcs_send_cmd(const uint8_t *cmd, size_t len){
     for(size_t i = 0; i < len; i++){
@@ -18,81 +19,42 @@ void adcs_send_cmd(const uint8_t *cmd, size_t len){
 int adcs_read_reply(uint8_t *response, size_t length){
     size_t i = 0;
     while(i < length){
-        if (uart_poll_in(adcs_uart, &response[i]) == 0){
-            i++;
-            printk("true");
+        int data_transmit = uart_poll_in(adcs_uart, &response[i]);
+        if (data_transmit == -1){
+            k_msleep(10);
+            printk("false");
         }
         else{
-            k_msleep(1);//wait for byte
-            printk("false");
+            i++;
+            printk("true");
         }
     }
     return i;
 }
 
+
 int main (void){
 
+    if (adcs_uart == NULL){
+        printk("null");
+    }else{
+        printk("device success");
+    }
+    printk("");
     printk("hi");
 
-    uint8_t cmd[] = { 0xC9, 0x01, 0x24, 0x00 };
+    uint8_t cmd[] = { 0xC9, 0x01, 0x03, 0x00, 0x33 }; //Test command: reads SNID of the ADCS. these need to be upadted still to access actual telemetry data
     adcs_send_cmd(cmd, sizeof(cmd));//sends a command to the ADCS, requesting to read all telemetry data based upon the above header bytes
 
-    uint8_t response[149];
+    uint8_t response[12];
 
-    adcs_read_reply(response, sizeof(response));
+    int length = adcs_read_reply(response, sizeof(response));
 
-    typedef struct {
-    // 6‐byte timestamp 
-    uint32_t time_s;
-    uint16_t time_subsec;
-    // magnetometer 
-    float mag_x;
-    float mag_y;
-    float mag_z;
-    // sun‐sensor unit vector (if digital sun sensors provide normalized output)
-    float sun_unit_x;
-    float sun_unit_y;
-    float sun_unit_z;
-    // accelerometer m/s^2
-    float accel_x;
-    float accel_y;
-    float accel_z;
-    // gyroscope (rad/s)
-    float gyro_x;
-    float gyro_y;
-    float gyro_z;
-    // reaction wheel speeds (RPM) and torques (mN·m)
-    float rw_speed_x;
-    float rw_speed_y;
-    float rw_speed_z;
-    float rw_torque_x;
-    float rw_torque_y;
-    float rw_torque_z;
-    // magnetorquer currents (A)
-    float mtq_current_x;
-    float mtq_current_y;
-    float mtq_current_z;
-    // temperatures ()
-    float temp_board;
-    float temp_rw_x;
-    float temp_rw_y;
-    float temp_rw_z;
-    // attitude quaternion (q0, q1, q2, q3)
-    float quat_w;
-    float quat_x;
-    float quat_y;
-    float quat_z;
-    // stored angular momentum (nms)
-    float momentum_x;
-    float momentum_y;
-    float momentum_z;
-    } adcs_telemetry_t;
+    //printing output (SNID):
+    for (int i = 0; i < length; i++){
+        printk("%c", response[i]);
+    }
 
-    adcs_telemetry_t telemetry;
-    memcpy(&telemetry, &response[4], sizeof(adcs_telemetry_t));
-
-    printk("%lf", telemetry.mag_x);
-    printk("hello");
     return 0;
 
 }
