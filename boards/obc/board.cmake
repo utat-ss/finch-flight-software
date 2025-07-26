6e7013924f909c0b9045ef817cb199dab0b47551
Alex Apostolu <apostolu240@gmail.com>
2025-07-26 16:14:26 -0400

    boards: obc: board.cmake: add stm32cubeprogrammer as runner
    Add STM32 Cube Programmer as a runner for OBC flashing.
    
    Signed-off-by: Alex Apostolu <apostolu240@gmail.com>

--- boards/obc/board.cmake
+++ boards/obc/board.cmake
@@ -2,5 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(pyocd "--target=stm32g431rbtx")
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
