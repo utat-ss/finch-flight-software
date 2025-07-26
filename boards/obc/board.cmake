26c9eccd70327c964467e5c81edc7ca4edbfa283
Rachel <rachelll.chen@mail.utoronto.ca>
2025-07-26 16:05:18 -0400

    fixed node connection to align with obc.dts

--- boards/obc/board.cmake
+++ boards/obc/board.cmake
@@ -2,7 +2,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(pyocd "--target=stm32g431rbtx")
+board_runner_args(openocd "--config=interface/stlink.cfg" "--config=target/stm32g4x.cfg")
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd-stm32.board.cmake)
--- lib/rtc-external/src/main.c
+++ lib/rtc-external/src/main.c
@@ -1,20 +1,68 @@
 /*
- * Copyright (c) 2025 The FINCH CubeSat Project Flight Software Contributors
+ * Copyright (c) 2024, Muhammad Waleed Badar
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/logging/log.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/rtc.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/sys/util.h>
 
-LOG_MODULE_REGISTER(obc);
+const struct device *rtc = DEVICE_DT_GET(DT_NODELABEL(mcp7940n));
+const struct device *uart = DEVICE_DT_GET(DT_NODELABEL(usart1));
+
+void print(const char* str)
+{
+	for (int i = 0; i < strlen(str); ++i) {
+		uart_poll_out(uart, str[i]);
+	}
+}
+
+static int set_date_time(const struct device *rtc)
+{
+	int ret = 0;
+	struct rtc_time tm = {
+		.tm_year = 2024 - 1900,
+		.tm_mon = 11 - 1,
+		.tm_mday = 17,
+		.tm_hour = 4,
+		.tm_min = 19,
+		.tm_sec = 0,
+	};
+
+	ret = rtc_set_time(rtc, &tm);
+	if (ret < 0) {
+		return ret;
+	}
+	return ret;
+}
 
 int main(void)
 {
-	while (1) {
-		LOG_INF("obc");
-    printk("obc wassup\n");
-		k_msleep(1000);
+	print("hello, zephyr!\r\n");
+		
+	/* Check if the RTC is ready */
+	if (!device_is_ready(rtc)) {
+		print("rip\r\n");
+		return 0;
 	}
+
+	set_date_time(rtc);
+
+	/* Continuously read the current date and time from the RTC */
+	while (1) {
+		struct rtc_time tm;
+		rtc_get_time(rtc, &tm);
+
+		char buf[64];
+		snprintf(buf, sizeof(buf), "time: %d\r\n", tm.tm_sec);
+		for (int i = 0; i < strlen(buf); ++i) {
+			uart_poll_out(uart, buf[i]);
+		}
+
+		k_msleep(1000);
+	};
 	return 0;
 }
