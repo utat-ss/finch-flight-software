bd421876553f146b90e2559ced5a7563dae6e437
Ihor Chovpan <i.chovpan@gmail.com>
2025-08-06 18:35:11 -0400

    CI: Add scripts for flashing and debugging.
    These scripts are meant to be run from a docker container. To run them,
    run `pyocd gdbserver` on the host with appropriate arguments.
    
    Signed-off-by: Ihor Chovpan <i.chovpan@gmail.com>

--- scripts/open_container.sh
+++ scripts/open_container.sh
@@ -5,6 +5,7 @@
 
 CONTAINER_NAME=utat-ss-finch-container
 IMAGE_NAME=utat-ss-finch
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
 container_exists() {
     docker ps -a --format '{{.Names}}' | grep -w "$CONTAINER_NAME" > /dev/null 2>&1
@@ -23,5 +24,5 @@ elif container_exists; then
     docker attach "$CONTAINER_NAME"
 else
     echo "Container '$CONTAINER_NAME' does not exist. Running a new container."
-    docker run -it --privileged -v $(pwd):/workspace:z -v /dev:/dev --name "$CONTAINER_NAME" "$IMAGE_NAME"
+    docker run -it -v "$SCRIPT_DIR"/..:/workspace:z --add-host="host.docker.internal:host-gateway" --name "$CONTAINER_NAME" "$IMAGE_NAME"
 fi
