ae79807c1f4bd1dd968fc032751f27c96317343a
Alex Apostolu <apostolu240@gmail.com>
2025-08-22 19:44:19 -0400

    ccsds123b: add missing copyright notices
    Add missing FINCH copyright to CCSDS123B files flagged by the
    copyright check script.
    
    Signed-off-by: Alex Apostolu <apostolu240@gmail.com>

--- lib/ccsds123b/src/image.c
+++ lib/ccsds123b/src/image.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2025 The FINCH CubeSat Project Flight Software Contributors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <finch/ccsds123b/image.h>
 
 vec3 N = (vec3){ 3, 4, 4 };
