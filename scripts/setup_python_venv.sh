b765f8a75bbcd8469e13fe1c9f172f4c8a663376
Mirai SHINJO <oss@mshinjo.com>
2025-08-23 13:58:57 +0900

    scripts: move pip install to setup_python_venv from setup_west_workspace
    The pip install step should be performed in setup_python_venv.sh, not in
    setup_west_workspace.sh. Additionally, use 'west packages' to install
    Python dependencies, rather than directly using 'pip install'.
    
    Signed-off-by: Mirai SHINJO <oss@mshinjo.com>

--- scripts/setup_python_venv.sh
+++ scripts/setup_python_venv.sh
@@ -9,3 +9,4 @@ source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../finch-flight-software-e
 python3 -m venv "$(dirname "${FINCH_FLIGHT_SOFTWARE_ROOT}")/.venv"
 source "$(dirname "${FINCH_FLIGHT_SOFTWARE_ROOT}")/.venv/bin/activate"
 pip install west
+west packages pip --install
