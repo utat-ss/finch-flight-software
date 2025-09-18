0c6f3a390b3f35cff10b2ed3da1cce9b9f73af00
Igor Chovpan <i.chovpan@gmail.com>
2025-09-18 17:21:43 -0400

    scripts: setup_west_workspace: download Zephyr SDK to zephyr-sdk folder
    This commit changes the download directory from "zephyr-sdk-x.xx.x" to
    "zephyr-sdk", making the name constant and predictable.
    
    This way scripts can access tools in the Zephyr SDK using a
    fixed path.
    
    Signed-off-by: Igor Chovpan <i.chovpan@gmail.com>

--- scripts/setup_west_workspace.sh
+++ scripts/setup_west_workspace.sh
@@ -11,4 +11,4 @@ cd "${FINCH_FLIGHT_SOFTWARE_ROOT}"
 west init --local --mf west.yml && west update
 west zephyr-export
 west packages pip --install
-west sdk install --install-base $(dirname "${FINCH_FLIGHT_SOFTWARE_ROOT}") --toolchains arm-zephyr-eabi
+west sdk install --install-dir $(dirname "${FINCH_FLIGHT_SOFTWARE_ROOT}")/zephyr-sdk --toolchains arm-zephyr-eabi
