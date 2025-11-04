82742d0fd59dd15e607ee72a8d752e72e1ab77e8
Alex Apostolu <apostolu240@gmail.com>
2025-11-03 20:39:18 -0500

    adcs: add adcs library
    Add ADCS library with init and get_id functions.
    
    Signed-off-by: Alex Apostolu <apostolu240@gmail.com>

---
+++ lib/adcs/src/adcs.c
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2025 The FINCH CubeSat Project Flight Software Contributors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <finch/adcs/adcs.h>
+
+#include <zephyr/drivers/uart.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/kernel.h>
+
+uint8_t const ADCS_ID_SIZE = 12;
+static uint8_t const adcs_id_expected[] =  { 0x54, 0x41, 0x44, 0x31, 0x30, 0x32, 0x30, 0x35, 0x33, 0x44, 0x00, 0x00 };
+
+/* ADCS is connected via USART2. */
+static const struct device *const adcs_uart = DEVICE_DT_GET(DT_NODELABEL(usart2));
+
+/* Global buffer to store ADCS RX bytes. */
+#define ADCS_RX_BUF_SIZE 32
+static uint8_t adcs_rx_buf[ADCS_RX_BUF_SIZE];
+static volatile uint8_t adcs_rx_len;
+static K_SEM_DEFINE(adcs_rx_sem, 0, 1);
+
+LOG_MODULE_REGISTER(adcs);
+
+static void adcs_uart_isr(const struct device *dev, void *user_data)
+{
+	ARG_UNUSED(dev);
+	ARG_UNUSED(user_data);
+
+	if (!uart_irq_update(dev)) {
+		return;
+	}
+
+	while (uart_irq_rx_ready(dev)) {
+		uint8_t received_byte;
+		uart_fifo_read(dev, &received_byte, 1);
+
+		if (adcs_rx_len < ADCS_RX_BUF_SIZE) {
+ 			adcs_rx_buf[adcs_rx_len++] = received_byte;
+ 		}
+
+		k_sem_give(&adcs_rx_sem);
+	}
+}
+
+adcs_rc_t adcs_init()
+{
+	if (!device_is_ready(adcs_uart)) {
+		return ADCS_RC_ERR;
+	}
+
+	uart_irq_callback_set(adcs_uart, adcs_uart_isr);
+	uart_irq_rx_enable(adcs_uart);
+
+	return ADCS_RC_OK;
+}
+
+adcs_rc_t adcs_get_id(uint8_t *id, uint8_t id_size)
+{
+	__ASSERT(id, "ID array cannot be NULL.");
+	__ASSERT(id_size == ADCS_ID_SIZE, "ID array must be exactly %u bytes.", ADCS_ID_SIZE);
+
+	k_sem_reset(&adcs_rx_sem);
+ 	adcs_rx_len = 0;
+
+	/* 5 byte command to read the ID. */
+	uint8_t adcs_id_cmd[] = { 0xc9, 0x01, 0x03, 0x00, 0x33 };
+
+	/* Send the command to read the ID. */
+	for (size_t i = 0; i < sizeof(adcs_id_cmd); ++i) {
+		uart_poll_out(adcs_uart, adcs_id_cmd[i]);
+	}
+
+	/* Wait for the ADCS to finish replying. */
+	k_timeout_t timeout = K_MSEC(1000);
+	while (k_sem_take(&adcs_rx_sem, timeout) == 0) {
+		timeout = K_MSEC(500);
+	}
+
+	if (adcs_rx_len < (4U + id_size)) {
+ 		LOG_ERR("ADCS ID response too short (%u bytes)", adcs_rx_len);
+ 		return ADCS_RC_ERR;
+ 	}
+
+	uint8_t adcs_rx_i = 0;
+
+	/* Verify the 4 byte RX header matches 4 byte command header. */
+	for (; adcs_rx_i < 4; ++adcs_rx_i) {
+		if (adcs_rx_buf[adcs_rx_i] != adcs_id_cmd[adcs_rx_i]) {
+			return ADCS_RC_ERR;
+		}
+	}
+
+	/* Output ID */
+	for (uint8_t i = 0; i < id_size; ++i, ++adcs_rx_i) {
+		if (adcs_rx_buf[adcs_rx_i] != adcs_id_expected[i]) {
+			/* TODO (?): return bitmap of where the ID failed to match with expected */
+			LOG_ERR("ADCS ID has unexpected value at position %u, 0x%02x", i, adcs_rx_buf[adcs_rx_i]);
+			return ADCS_RC_ERR;
+		}
+
+		id[i] = adcs_rx_buf[adcs_rx_i];
+	}
+
+	/* TODO: Verify checksum. */
+
+	return ADCS_RC_OK;
+}
