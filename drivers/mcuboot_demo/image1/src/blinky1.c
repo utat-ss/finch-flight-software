2727482b19532e7256d8c058d79514a1759ea17e
Joy(Jiayi) Xu <joyx417@gmail.com>
2025-11-22 22:00:35 +0000

    mcuboot: Enable automatic blinky swap.
    Updated the blinky1.c file, changed manually reset button function into auto-swap after 5 sec to blinky2.c.
    
    Signed-off-by: Joy(Jiayi) Xu <joyx417@gmail.com>

--- drivers/mcuboot_demo/image1/src/blinky1.c
+++ drivers/mcuboot_demo/image1/src/blinky1.c
@@ -1,52 +1,57 @@
+/* Copyright (c) 2025 The FINCH CubeSat Project Flight Software Contributors*/
+/* SPDX-License-Identifier: Apache-2.0 */
+
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/dfu/mcuboot.h>
 #include <zephyr/sys/reboot.h>
 
 #define LED_NODE DT_ALIAS(led0)
-#define SW0_NODE DT_ALIAS(sw0)
-
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET_OR(LED_NODE, gpios, {0});
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios, {0});
-static struct gpio_callback button_cb;
-
-void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
-{
-    printk("Button pressed! Requesting upgrade to slow blinky...\n");
-
-    // Request upgrade - use TEST mode so it reverts if not confirmed
-    int ret = boot_request_upgrade(BOOT_UPGRADE_TEST);
-    if (ret) {
-        printk("Failed to request upgrade: %d\n", ret);
-        return;
-    }
-
-    printk("Upgrade requested, rebooting in next reset...\n");
-
-    // could command a system reboot on button press or hold off until next reset
-
-    // k_msleep(100);
-    // sys_reboot(SYS_REBOOT_COLD);
-}
 
 void main(void)
 {
-    if (!boot_is_img_confirmed()) {
-        printk("Confirming image...\n");
-        boot_write_img_confirmed();
-    }
-    
-    gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	// 1. Initialization
+	// If this image just booted successfully, mark it as "Good" so we don't revert.
+	if (!boot_is_img_confirmed()) {
+		printk("Confirming image...\n");
+		boot_write_img_confirmed();
+	}
 
-    gpio_pin_configure_dt(&button, GPIO_INPUT);
-    gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
-    gpio_init_callback(&button_cb, button_pressed, BIT(button.pin));
-    gpio_add_callback(button.port, &button_cb);
+	gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 
-    printk("Running fast blinky. Press button to swap.\n");
+	printk("Running FAST blinky (Image 1).\n");
+	printk("System will auto-swap in approx 10 seconds...\n");
 
-    while (1) {
-        gpio_pin_toggle_dt(&led);
-        k_msleep(200);  // Fast blink: 0.2 sec
-    }
+	int blink_count = 0;
+	int trigger_threshold = 50; // 50 * 200ms = 10 seconds
+
+	// 2. The Main Loop
+	while (1) {
+		gpio_pin_toggle_dt(&led);
+		k_msleep(200); // Fast blink
+
+		blink_count++;
+
+		// 3. The Automatic Trigger Check
+		if (blink_count == trigger_threshold) {
+			printk("Time's up! Requesting upgrade to slow blinky...\n");
+
+			// A. Request the swap (Test Mode)
+			// 'BOOT_UPGRADE_TEST' ensures that if the new image crashes,
+			// the bootloader will swap BACK to this one automatically.
+			int ret = boot_request_upgrade(BOOT_UPGRADE_TEST);
+
+			if (ret == 0) {
+				printk("Upgrade requested successfully. Rebooting now!\n");
+				// Give the serial port a moment to flush the text
+				k_msleep(100);
+
+				// B. Software Reset (Simulates the Black Button)
+				sys_reboot(SYS_REBOOT_COLD);
+			} else {
+				printk("Failed to request upgrade: %d\n", ret);
+			}
+		}
+	}
 }
--- drivers/mcuboot_demo/image2/src/blinky2.c
+++ drivers/mcuboot_demo/image2/src/blinky2.c
@@ -1,6 +1,6 @@
-#include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/dfu/mcuboot.h>
+/* Copyright (c) 2025 The FINCH CubeSat Project Flight Software Contributors*/
+/* SPDX-License-Identifier: Apache-2.0 */
+
 #include <zephyr/sys/reboot.h>
 
 #define LED_NODE DT_ALIAS(led0)
@@ -12,40 +12,40 @@ static struct gpio_callback button_cb;
 
 void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
-    printk("Button pressed! Requesting upgrade to fast blinky...\n");
+	printk("Button pressed! Requesting upgrade to fast blinky...\n");
 
-    int ret = boot_request_upgrade(BOOT_UPGRADE_TEST);
-    if (ret) {
-        printk("Failed to request upgrade: %d\n", ret);
-        return;
-    }
+	int ret = boot_request_upgrade(BOOT_UPGRADE_TEST);
+	if (ret) {
+		printk("Failed to request upgrade: %d\n", ret);
+		return;
+	}
 
-    printk("Upgrade requested, rebooting in next reset...\n");
+	printk("Upgrade requested, rebooting in next reset...\n");
 
-    // could command a system reboot on button press or hold off until next reset
-    
-    // k_msleep(100);
-    // sys_reboot(SYS_REBOOT_COLD);
+	// could command a system reboot on button press or hold off until next reset
+
+	// k_msleep(100);
+	// sys_reboot(SYS_REBOOT_COLD);
 }
 
 void main(void)
 {
-    if (!boot_is_img_confirmed()) {
-        printk("Confirming image...\n");
-        boot_write_img_confirmed();
-    }
-    
-    gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (!boot_is_img_confirmed()) {
+		printk("Confirming image...\n");
+		boot_write_img_confirmed();
+	}
 
-    gpio_pin_configure_dt(&button, GPIO_INPUT);
-    gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
-    gpio_init_callback(&button_cb, button_pressed, BIT(button.pin));
-    gpio_add_callback(button.port, &button_cb);
+	gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 
-    printk("Running slow blinky. Press button to swap.\n");
+	gpio_pin_configure_dt(&button, GPIO_INPUT);
+	gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+	gpio_init_callback(&button_cb, button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb);
 
-    while (1) {
-        gpio_pin_toggle_dt(&led);
-        k_msleep(1000);  // Slow blink: 1 sec
-    }
+	printk("Running slow blinky. Press button to swap.\n");
+
+	while (1) {
+		gpio_pin_toggle_dt(&led);
+		k_msleep(1000); // Slow blink: 1 sec
+	}
 }
