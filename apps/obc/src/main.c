266fe8421b010f7ebcc4256360e76731fea65a86
Alex Apostolu <apostolu240@gmail.com>
2025-11-29 11:59:22 -0500

    test commit
    Signed-off-by: Alex Apostolu <apostolu240@gmail.com>

--- apps/obc/src/main.c
+++ apps/obc/src/main.c
@@ -4,16 +4,33 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/kernel.h>
+ #include <zephyr/kernel.h>
+ #include <zephyr/drivers/can.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(obc);
 
+const struct device *const can = DEVICE_DT_GET(DT_CHOSEN(zephyr_canbus));
+
 int main(void)
 {
-	while (1) {
-		LOG_INF("obc");
-		k_msleep(1000);
+	int ret;
+	struct can_frame frame = {
+		.flags = 0,
+		.id = 0x123,
+		.dlc = 8,
+		.data = { 1, 2, 3, 4, 5, 6, 7, 8 }
+	};
+
+	LOG_INF("Starting CAN send");
+
+	/* 3000ms timeout */
+	ret = can_send(can, &frame, K_MSEC(3000), NULL, NULL);
+	if (ret) {
+		LOG_ERR("Sending failed [%d]", ret);
+		return 0;
 	}
+
+	LOG_INF("CAN sent successfully");
 	return 0;
 }
