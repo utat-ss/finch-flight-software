4f701e6e358a8d335c9a4e1de4bf268a8fff71c4
Mirai SHINJO <oss@mshinjo.com>
2026-03-14 10:12:17 -0400

    lib: csp: add FINCH CSP integration layer
    Bring up libcsp for FINCH.
    
    Add the finch_csp_init() API, start the router thread,
    and bind the built-in CSP service handlers.
    
    This does not add an external transport yet.
    
    Signed-off-by: Mirai SHINJO <oss@mshinjo.com>

---
+++ include/finch/csp/csp.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2026 The FINCH CubeSat Project Flight Software Contributors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef FINCH_CSP_CSP_H_
+#define FINCH_CSP_CSP_H_
+
+int finch_csp_init(void);
+
+#endif /* FINCH_CSP_CSP_H_ */
