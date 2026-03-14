ac0951459c2d3078c142a22f9a8ddaf4d4c8cc01
Mirai SHINJO <oss@mshinjo.com>
2026-03-14 10:17:03 -0400

    apps: obc: enable FINCH CSP integration
    Enable FINCH CSP for OBC.
    
    Set the node address and hostname, and initialize CSP
    at app startup.
    
    Signed-off-by: Mirai SHINJO <oss@mshinjo.com>

--- apps/obc/src/main.c
+++ apps/obc/src/main.c
@@ -4,6 +4,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <finch/adcs/adcs.h>
+#include <finch/csp/csp.h>
+
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
@@ -11,9 +14,33 @@ LOG_MODULE_REGISTER(obc);
 
 int main(void)
 {
-	while (1) {
-		LOG_INF("obc");
-		k_msleep(1000);
+	int ret = finch_csp_init();
+
+	if (ret < 0) {
+		LOG_ERR("Failed to initialize FINCH CSP (%d)", ret);
+		return ret;
 	}
-	return 0;
+
+    if (!device_is_ready(can_dev)) {
+        LOG_ERR("CAN device not ready");
+        return -ENODEV;
+    }
+
+    can_start(can_dev);
+
+    const struct can_filter pi_filter = {
+        .flags = 0U,
+        .id = CAN_OBC_ID,
+        .mask = CAN_STD_ID_MASK
+    };
+
+    can_add_rx_filter(can_dev, pi_rx_callback, NULL, &pi_filter);
+
+    while (1) {
+        if (!k_sem_take(&rx_sem, K_FOREVER)) {
+            handle_can_command(&rx_frame_storage);
+        }
+    }
+
+    return 0;
 }
