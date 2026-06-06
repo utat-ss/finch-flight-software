0ac4f422fae61450cccd0b3523a16248b3b6e077
Alex Apostolu <apostolu240@gmail.com>
2026-06-05 23:49:29 -0400

    adcs: init adcs and verify id in main
    Init ADCS and verify ID in main.
    
    Signed-off-by: Alex Apostolu <apostolu240@gmail.com>

--- apps/obc/src/main.c
+++ apps/obc/src/main.c
@@ -4,25 +4,45 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <finch/adcs/adcs.h>
 #include <finch/csp/csp.h>
-#include <zephyr/kernel.h>
+
 #include <zephyr/logging/log.h>
+#include <zephyr/kernel.h>
 
 LOG_MODULE_REGISTER(obc);
 
 int main(void)
 {
-	int ret = finch_csp_init();
+	int rc;
+	adcs_rc_t adcs_rc;
 
-	if (ret < 0) {
-		/* TODO FDIR must handle this situation! */
-		LOG_ERR("Failed to initialize FINCH CSP (%d)", ret);
-		return ret;
+	rc = finch_csp_init();
+	if (rc < 0) {
+		LOG_ERR("Failed to initialize FINCH CSP (%d)", rc);
+		return rc;
 	}
+
+	adcs_rc = adcs_init();
+	if (adcs_rc != ADCS_RC_OK) {
+		LOG_ERR("Failed to initialize ADCS (%d)", adcs_rc);
+		return adcs_rc;
+	}
+
+	/* Verify ADCS ID. */
+
+	uint8_t adcs_id[ADCS_ID_SIZE];
+	adcs_rc = adcs_get_id(adcs_id, ADCS_ID_SIZE);
+
+	LOG_RAW("ADCS ID: ");
+	for (uint8_t i = 0; i < ADCS_ID_SIZE; ++i) {
+		LOG_RAW("0x%02x ", adcs_id[i]);
+	}
+	LOG_RAW("\n");
 
 	while (1) {
-		LOG_INF("obc");
 		k_msleep(1000);
 	}
+
 	return 0;
 }
